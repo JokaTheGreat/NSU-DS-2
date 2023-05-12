0ac24cf3c18a5e089a01120b40a1b52981bf3a9d
Nikita Latysh <n.latysh@g.nsu.ru>
2023-05-12 23:03:36 +0700

    Add timeout

--- NSU-DS-2-manager/src/main/java/ru/nlatysh/manager/ManagerService.java
+++ NSU-DS-2-manager/src/main/java/ru/nlatysh/manager/ManagerService.java
@@ -6,11 +6,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import ru.nlatysh.common.models.CrackHashManagerRequest;
 import ru.nlatysh.common.models.CrackHashWorkerResponse;
 import ru.nlatysh.manager.models.CrackHash;
 import ru.nlatysh.manager.models.CrackHashStatus;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -19,6 +21,7 @@ import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class ManagerService {
+    private final int TIMEOUT = 30;
     private final ConcurrentMap<String, CrackHashStatus> crackHashJobs;
     private final CrackHashManagerRequest.Alphabet alphabet;
     private static final int workersCount = 1;
@@ -27,14 +30,15 @@ public class ManagerService {
         crackHashJobs = new ConcurrentHashMap<>();
 
         alphabet = new CrackHashManagerRequest.Alphabet();
-        List<String> alphabetList = Arrays.asList("a", "b", "c", "d",
-                "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
-                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        List<String> alphabetList = Arrays.asList("abcdefghijklmnopqrstuvwxyz0123456789".split(""));
         alphabet.getSymbols().addAll(alphabetList);
     }
 
     public ResponseEntity<String> crackHash(CrackHash crackHash) {
+        if (crackHash.maxLength < 1) {
+            return ResponseEntity.badRequest().build();
+        }
+
         String requestId = UUID.randomUUID().toString();
         crackHashJobs.put(requestId, new CrackHashStatus());
 
@@ -53,6 +57,10 @@ public class ManagerService {
 
     public ResponseEntity<Void> patchCrackHashData(CrackHashWorkerResponse crackHashResponse) {
         var currentCrackHashJob = crackHashJobs.get(crackHashResponse.getRequestId());
+        if (currentCrackHashJob.status == CrackHashStatus.Status.ERROR) {
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+        }
+
         currentCrackHashJob.status = CrackHashStatus.Status.READY;
         currentCrackHashJob.data = crackHashResponse.getAnswers().getWords();
         return ResponseEntity.ok().build();
@@ -75,6 +83,11 @@ public class ManagerService {
                 .bodyValue(crackHashRequest)
                 .retrieve()
                 .bodyToMono(ResponseEntity.class)
+                .timeout(Duration.ofSeconds(TIMEOUT))
+                .onErrorResume(error -> {
+                    crackHashJobs.get(requestId).status = CrackHashStatus.Status.ERROR;
+                    return Mono.just(ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build());
+                })
                 .toFuture();
     }
 }
